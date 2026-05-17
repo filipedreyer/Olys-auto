e18f77ef74c133bac2e2e6caa19eb06454fc6ee1
filipedreyer <filipedreyer@gmail.com>
2026-05-17 14:39:51 -0300

    feat: connect Hoje screen to application

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
+import { HojeScreen } from '../features/fazer/screens/HojeScreen'
 import { AppShell } from './shell/AppShell'
 
 export function App() {
   return (
     <AppShell>
-      <section>
-        <h1>Hoje</h1>
-        <p>Operational execution surface initialized.</p>
-      </section>
+      <HojeScreen />
     </AppShell>
   )
 }
